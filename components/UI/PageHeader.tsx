91deec0c198098e20b74ace8c52e42f77b466316
Theedon <faniyantoyin@gmail.com>
2023-10-30 14:39:51 +0100

    feat(top-bar): add top bar to project

---
+++ components/UI/PageHeader.tsx
@@ -0,0 +1,113 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Menu,
+  Video,
+  BellDot,
+  User,
+  Search,
+  Mic,
+  ArrowLeft,
+} from "lucide-react";
+import { Button } from "@/components";
+import { useState } from "react";
+function PageHeader() {
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+  return (
+    <div
+      onClick={() => {
+        setShowFullWidthSearch(false);
+      }}
+      className="mx-4 mb-6 flex w-full justify-between gap-10 pt-2 lg:gap-20"
+    >
+      <div
+        className={`flex flex-shrink-0 items-center gap-4 ${
+          showFullWidthSearch ? "hidden" : "flex"
+        } `}
+      >
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+
+        <Link className="flex" href="/">
+          <Image
+            className="mr-1"
+            src="/youtubeLogo.png"
+            alt="youtube-logo"
+            height={20}
+            width={20}
+          ></Image>{" "}
+          Youtube
+        </Link>
+      </div>
+
+      <form
+        className={`flex-grow justify-end gap-4 md:flex md:justify-center ${
+          showFullWidthSearch ? "flex" : "hidden md:flex"
+        }`}
+      >
+        <Button
+          type="button"
+          onClick={() => {
+            setShowFullWidthSearch(false);
+          }}
+          variant="ghost"
+          className={`${showFullWidthSearch ? "flex" : "hidden"}`}
+        >
+          <ArrowLeft />
+        </Button>
+        <div className="flex max-w-[600px] flex-grow">
+          <input
+            onClick={(event) => {
+              event?.stopPropagation();
+            }}
+            type="search"
+            placeholder="search"
+            className=" w-full rounded-l-full  border border-secondary-border px-4 py-1 shadow-inner shadow-secondary outline-none focus:border-blue-500"
+          />
+          <Button
+            type="button"
+            className="flex-shrink-0 rounded-r-full border border-l-0 border-secondary-border px-4 py-2"
+          >
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+
+      <div
+        className={`flex flex-shrink-0 md:gap-2 ${
+          showFullWidthSearch ? "hidden" : "flex"
+        } `}
+      >
+        <Button
+          onClick={(event) => {
+            event.stopPropagation();
+            setShowFullWidthSearch(true);
+          }}
+          variant="ghost"
+          className="md:hidden"
+        >
+          <Search />
+        </Button>
+        <Button variant="ghost" className="md:hidden">
+          <Mic />
+        </Button>
+        <Button variant="ghost">
+          <Video />
+        </Button>
+        <Button variant="ghost">
+          <BellDot />
+        </Button>
+        <Button variant="ghost">
+          <User />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default PageHeader;
